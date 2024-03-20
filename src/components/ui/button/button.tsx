c0785b9d6d51cf88f09006c4c52055d68bf7b2f9
Marina Kudlatskaya <m.kudlatskaya@gmail.com>
2024-03-20 19:23:02 +0300

    button components refactoring

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -3,12 +3,22 @@ import { ComponentPropsWithoutRef } from 'react'
 import s from './button.module.scss'
 
 export type ButtonProps = {
+  as: any
   fullWidth?: boolean
-  variant?: 'link' | 'primary' | 'secondary' | 'tertiary'
+  variant?: 'primary' | 'secondary'
 } & ComponentPropsWithoutRef<'button'>
 
-export const Button = ({ className, fullWidth, variant = 'primary', ...rest }: ButtonProps) => {
+export const Button = ({
+  as: Component = 'button',
+  className,
+  fullWidth,
+  variant = 'primary',
+  ...rest
+}: ButtonProps) => {
   return (
-    <button className={`${s[variant]} ${fullWidth ? s.fullWidth : ''} ${className}`} {...rest} />
+    <Component
+      className={`${s.button} ${s[variant]} ${fullWidth ? s.fullWidth : ''} ${className}`}
+      {...rest}
+    />
   )
 }
