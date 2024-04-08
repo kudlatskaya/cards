37a7ff1251b6afb83c73c9687069e8fed59791c1
Marina Kudlatskaya <m.kudlatskaya@gmail.com>
2024-04-08 20:44:47 +0300

    table component created

--- src/App.tsx
+++ src/App.tsx
@@ -6,37 +6,74 @@ import s from './components/ui/button/button.module.scss'
 import {Card} from "@/components/ui/card/card";
 import {Table} from "@/components/ui/table";
 
+const val = {
+    heads: ['name', 'name', 'name', 'name', 'name', 'name',],
+    values: [{
+        "questionImg": "string",
+        "question": "string",
+        "answer": "string",
+        "deckId": "string",
+        "userId": "string",
+        "grade": 0,
+        "id": "string",
+        "shots": 0,
+        "answerImg": "string",
+        "questionVideo": "string",
+        "answerVideo": "string",
+        "created": "2024-04-08T13:02:38.782Z",
+        "updated": "2024-04-08T13:02:38.782Z"
+    },
+        {
+            "grade": 0,
+            "id": "string",
+            "deckId": "string",
+            "userId": "string",
+            "question": "string",
+            "answer": "string",
+            "shots": 0,
+            "answerImg": "string",
+            "questionImg": "string",
+            "questionVideo": "string",
+            "answerVideo": "string",
+            "created": "2024-04-08T13:02:38.782Z",
+            "updated": "2024-04-08T13:02:38.782Z"
+        }]
+}
+
+
+
 export function App() {
   return (
       <div>
-        <div>
-          <Table/>
-        </div>
-        {/*<Button as={'div'}>Normal Button</Button>*/}
-        <div>
-          <Button as={'a'} href={'https://google.com'}>
-            Link that looks a button
-          </Button>
-          <Button disabled={false} fullWidth={false} variant={'primary'}>
-            <FiLogOut className={s.icon}/>
-            Primary button
-          </Button>
-          <Button variant={'secondary'}>Secondary button</Button>
-        </div>
+          <div>
+              <Table data={val}/>
+          </div>
+          {/*<Button as={'div'}>Normal Button</Button>*/}
+          <div>
+              <Button as={'a'} href={'https://google.com'}>
+                  Link that looks a button
+              </Button>
+              <Button disabled={false} fullWidth={false} variant={'primary'}>
+                  <FiLogOut className={s.icon}/>
+                  Primary button
+              </Button>
+              <Button variant={'secondary'}>Secondary button</Button>
+          </div>
 
-        <div>
-          <Input disabled fullWidth={false} label={'Default input'} variant={'default'}/>
-          <Input disabled={false} fullWidth={false} label={'Default input'} variant={'default'}/>
-          <Input disabled={false} fullWidth={false} label={'Password input'} variant={'password'}/>
-          <Input disabled fullWidth={false} label={'Password input'} variant={'password'}/>
-          <Input fullWidth={false} label={'Search input'} variant={'search'}/>
-          <Input disabled fullWidth={false} label={'Search input'} variant={'search'}/>
-          <Input fullWidth={false} label={'Search input'} variant={'search'} errorMessage={'Error'}/>
-          <Input disabled fullWidth={true} label={'Search input'} variant={'search'}/>
-        </div>
-        <div>
-          <Card/>
-        </div>
+          <div>
+              <Input disabled fullWidth={false} label={'Default input'} variant={'default'}/>
+              <Input disabled={false} fullWidth={false} label={'Default input'} variant={'default'}/>
+              <Input disabled={false} fullWidth={false} label={'Password input'} variant={'password'}/>
+              <Input disabled fullWidth={false} label={'Password input'} variant={'password'}/>
+              <Input fullWidth={false} label={'Search input'} variant={'search'}/>
+              <Input disabled fullWidth={false} label={'Search input'} variant={'search'}/>
+              <Input fullWidth={false} label={'Search input'} variant={'search'} errorMessage={'Error'}/>
+              <Input disabled fullWidth={true} label={'Search input'} variant={'search'}/>
+          </div>
+          <div>
+              <Card/>
+          </div>
+
 
       </div>
   )
--- src/components/ui/table/table.tsx
+++ src/components/ui/table/table.tsx
@@ -1,28 +1,61 @@
 import s from './table.module.scss'
 
-export type Props = {}
+type Data = {
+    heads: string[],
+    values: Value[]
+}
+
+type Value = {
+    questionImg: string,
+    question: string,
+    answer: string,
+    deckId: string,
+    userId: string,
+    grade: number,
+    id: string,
+    shots: number,
+    answerImg: string,
+    questionVideo: string,
+    answerVideo: string,
+    created: string,
+    updated: string
+
+}
+
+export type Props = {
+    data: Data
+}
 
 export const Table = (props: Props) => {
-    const {  } = props
+    const { data } = props
+    // const columnCount =  data.heads.length;
 
     return (
-        <div className={s.table}>
-            <div className={s.head}>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-            </div>
-            <div className={s.body}>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-            </div>
-        </div>
+        <table className={s.table}>
+            {/*<div className={s.column}>*/}
+            {/*    {*/}
+            {/*        data.heads.map(head => <div>{head}</div>)*/}
+            {/*    }*/}
+            {/*</div>*/}
+            <tr className={s.head}>
+                {
+                    data.heads.map(head => <th>{head}</th>)
+                }
+            </tr>
+
+                {
+                    data.values.map(value =>
+                        <tr className={s.row}>
+                            <td><img src={value.questionImg} /></td>
+                            <td> {value.questionVideo}</td>
+                            <td> {value.answer}</td>
+                            <td> {value.shots}</td>
+                            <td> {value.grade}</td>
+                            <td> {'icons'}</td>
+                        </tr>
+                    )
+                }
+
+        </table>
     )
 }
