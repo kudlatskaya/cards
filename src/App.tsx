a765dcdcad9d874bb8b056c860e51c6bc69d26d0
Marina Kudlatskaya <m.kudlatskaya@gmail.com>
2024-05-06 20:11:05 +0300

    react hook form install

--- src/App.tsx
+++ src/App.tsx
@@ -1,171 +1,195 @@
-import SvgClose from '@/assets/icons/components/close'
-import SvgEye from '@/assets/icons/components/eye'
-import SvgLogout from '@/assets/icons/components/logout'
-import SvgSearch from '@/assets/icons/components/search'
-import SvgStar from '@/assets/icons/components/star'
-import image from '@/assets/img/image.png'
-import { Button } from '@/components/ui/button'
-import { Card } from '@/components/ui/card/card'
-import { CheckboxComponent } from '@/components/ui/checkbox/checkbox'
-import { Input } from '@/components/ui/input/input'
-import Rating from '@/components/ui/rating/rating'
-import { Table } from '@/components/ui/table'
-import { Typography } from '@/components/ui/typography/typography'
+// import SvgClose from '@/assets/icons/components/close'
+// import SvgEye from '@/assets/icons/components/eye'
+// import SvgLogout from '@/assets/icons/components/logout'
+// import SvgSearch from '@/assets/icons/components/search'
+// import SvgStar from '@/assets/icons/components/star'
+// import image from '@/assets/img/image.png'
+// import { Button } from '@/components/ui/button'
+// import { Card } from '@/components/ui/card/card'
+// import { CheckboxComponent } from '@/components/ui/checkbox/checkbox'
+// import { Input } from '@/components/ui/input/input'
+// import Rating from '@/components/ui/rating/rating'
+// import { Table } from '@/components/ui/table'
+// import { Typography } from '@/components/ui/typography/typography'
 
 export function App() {
 	// @ts-ignore
 	return (
 		<div>
-			<div>
-				<Table.Root>
-					<Table.Head>
-						<Table.Tr>
-							<Table.Th>{'Name'}</Table.Th>
-							<Table.Th>{'Name'}</Table.Th>
-							<Table.Th>{'Name'}</Table.Th>
-							<Table.Th>{'Name'}</Table.Th>
-							<Table.Th>{'Name'}</Table.Th>
-						</Table.Tr>
-					</Table.Head>
-					<Table.Body>
-						<Table.Tr>
-							<Table.Td>
-								<div>
-									<img src={image} />
-									{`Name`}
-								</div>
-							</Table.Td>
-							<Table.Td>{'Name'}</Table.Td>
-							<Table.Td>{'Name'}</Table.Td>
-							<Table.Td>{'Name'}</Table.Td>
-							<Table.Td>
-								<Rating />
-							</Table.Td>
-						</Table.Tr>
-					</Table.Body>
-				</Table.Root>
+			<div
+				style={{
+					width: '50%',
+				}}
+			>
+				<form
+					method={'POST'}
+					style={{
+						color: 'black',
+						display: 'flex',
+						flexDirection: 'column',
+						gap: 24,
+					}}
+				>
+					<input name={'userEmail'} type={'email'} />
+					<input name={'userPassword'} type={'password'} />
+					<input name={'rememberMe'} type={'checkbox'} />
+					<input name={'favoriteColor'} type={'color'} />
+					<input name={'dob'} type={'date'} />
+					<button style={{ backgroundColor: 'white' }} type={'submit'}>
+						Submit
+					</button>
+				</form>
 			</div>
+			{/*<div>*/}
+			{/*	<Table.Root>*/}
+			{/*		<Table.Head>*/}
+			{/*			<Table.Tr>*/}
+			{/*				<Table.Th>{'Name'}</Table.Th>*/}
+			{/*				<Table.Th>{'Name'}</Table.Th>*/}
+			{/*				<Table.Th>{'Name'}</Table.Th>*/}
+			{/*				<Table.Th>{'Name'}</Table.Th>*/}
+			{/*				<Table.Th>{'Name'}</Table.Th>*/}
+			{/*			</Table.Tr>*/}
+			{/*		</Table.Head>*/}
+			{/*		<Table.Body>*/}
+			{/*			<Table.Tr>*/}
+			{/*				<Table.Td>*/}
+			{/*					<div>*/}
+			{/*						<img src={image} />*/}
+			{/*						{`Name`}*/}
+			{/*					</div>*/}
+			{/*				</Table.Td>*/}
+			{/*				<Table.Td>{'Name'}</Table.Td>*/}
+			{/*				<Table.Td>{'Name'}</Table.Td>*/}
+			{/*				<Table.Td>{'Name'}</Table.Td>*/}
+			{/*				<Table.Td>*/}
+			{/*					<Rating />*/}
+			{/*				</Table.Td>*/}
+			{/*			</Table.Tr>*/}
+			{/*		</Table.Body>*/}
+			{/*	</Table.Root>*/}
+			{/*</div>*/}
 
-			<div>
-				<Typography.H1 color={'light'}>
-					{'Lorem ipsum dolor sit amet, consectetur adipiscing elit'}{' '}
-				</Typography.H1>
-				<Typography.H2 color={'light'}>
-					{'Lorem ipsum dolor sit amet, consectetur adipiscing elit'}{' '}
-				</Typography.H2>
-				<Typography.Body1 color={'light'}>
-					{'Lorem ipsum dolor sit amet, consectetur adipiscing elit'}{' '}
-				</Typography.Body1>
-				<Typography.Body2 color={'light'}>
-					{'Lorem ipsum dolor sit amet, consectetur adipiscing elit'}{' '}
-				</Typography.Body2>
-				<Typography.Caption color={'light'}>
-					{'Lorem ipsum dolor sit amet, consectetur adipiscing elit'}{' '}
-				</Typography.Caption>
-				<Typography.H3 color={'light'}>
-					{'Lorem ipsum dolor sit amet, consectetur adipiscing elit'}{' '}
-				</Typography.H3>
-				<Typography.H4 color={'light'}>
-					{'Lorem ipsum dolor sit amet, consectetur adipiscing elit'}{' '}
-				</Typography.H4>
-				<Typography.Link1 color={'light'}>
-					{'Lorem ipsum dolor sit amet, consectetur adipiscing elit'}{' '}
-				</Typography.Link1>
-				<p>
-					<Typography.Link2 color={'light'}>
-						{'Lorem ipsum dolor sit amet, consectetur adipiscing elit'}{' '}
-					</Typography.Link2>
-				</p>
-				<Typography.Overline color={'light'}>
-					{'Lorem ipsum dolor sit amet, consectetur adipiscing elit'}{' '}
-				</Typography.Overline>
-				<Typography.Subtitle1 color={'light'}>
-					{'Lorem ipsum dolor sit amet, consectetur adipiscing elit'}{' '}
-				</Typography.Subtitle1>
-				<Typography.Subtitle2 color={'light'}>
-					{'Lorem ipsum dolor sit amet, consectetur adipiscing elit'}{' '}
-				</Typography.Subtitle2>
-			</div>
-			<div>
-				{/*<Button as={'a'} href={'https://google.com'}>*/}
-				{/*	Link that looks a button*/}
-				{/*</Button>*/}
-				<Button disabled={false} fullWidth={false} icon={<SvgLogout />} variant={'primary'}>
-					Primary button
-				</Button>
-				{/*<Button variant={'secondary'}>Secondary button</Button>*/}
-			</div>
-			<div>
-				<Input as={'textarea'} label={'Default input'} variant={'default'} />
-				<Input disabled fullWidth={false} label={'Default input'} variant={'default'} />
-				<Input disabled={false} fullWidth={false} label={'Default input'} variant={'default'} />
-				<Input
-					disabled={false}
-					errorMessage={'Error'}
-					fullWidth={false}
-					label={'Default input'}
-					variant={'default'}
-				/>
-				<span>{'ku'}</span>
-				<Input
-					disabled={false}
-					fullWidth
-					iconEnd={<SvgEye />}
-					label={'Password input'}
-					variant={'password'}
-				/>
-				<Input
-					disabled
-					fullWidth={false}
-					iconEnd={<SvgEye />}
-					label={'Password input'}
-					variant={'password'}
-				/>
+			{/*<div>*/}
+			{/*	<Typography.H1 color={'light'}>*/}
+			{/*		{'Lorem ipsum dolor sit amet, consectetur adipiscing elit'}{' '}*/}
+			{/*	</Typography.H1>*/}
+			{/*	<Typography.H2 color={'light'}>*/}
+			{/*		{'Lorem ipsum dolor sit amet, consectetur adipiscing elit'}{' '}*/}
+			{/*	</Typography.H2>*/}
+			{/*	<Typography.Body1 color={'light'}>*/}
+			{/*		{'Lorem ipsum dolor sit amet, consectetur adipiscing elit'}{' '}*/}
+			{/*	</Typography.Body1>*/}
+			{/*	<Typography.Body2 color={'light'}>*/}
+			{/*		{'Lorem ipsum dolor sit amet, consectetur adipiscing elit'}{' '}*/}
+			{/*	</Typography.Body2>*/}
+			{/*	<Typography.Caption color={'light'}>*/}
+			{/*		{'Lorem ipsum dolor sit amet, consectetur adipiscing elit'}{' '}*/}
+			{/*	</Typography.Caption>*/}
+			{/*	<Typography.H3 color={'light'}>*/}
+			{/*		{'Lorem ipsum dolor sit amet, consectetur adipiscing elit'}{' '}*/}
+			{/*	</Typography.H3>*/}
+			{/*	<Typography.H4 color={'light'}>*/}
+			{/*		{'Lorem ipsum dolor sit amet, consectetur adipiscing elit'}{' '}*/}
+			{/*	</Typography.H4>*/}
+			{/*	<Typography.Link1 color={'light'}>*/}
+			{/*		{'Lorem ipsum dolor sit amet, consectetur adipiscing elit'}{' '}*/}
+			{/*	</Typography.Link1>*/}
+			{/*	<p>*/}
+			{/*		<Typography.Link2 color={'light'}>*/}
+			{/*			{'Lorem ipsum dolor sit amet, consectetur adipiscing elit'}{' '}*/}
+			{/*		</Typography.Link2>*/}
+			{/*	</p>*/}
+			{/*	<Typography.Overline color={'light'}>*/}
+			{/*		{'Lorem ipsum dolor sit amet, consectetur adipiscing elit'}{' '}*/}
+			{/*	</Typography.Overline>*/}
+			{/*	<Typography.Subtitle1 color={'light'}>*/}
+			{/*		{'Lorem ipsum dolor sit amet, consectetur adipiscing elit'}{' '}*/}
+			{/*	</Typography.Subtitle1>*/}
+			{/*	<Typography.Subtitle2 color={'light'}>*/}
+			{/*		{'Lorem ipsum dolor sit amet, consectetur adipiscing elit'}{' '}*/}
+			{/*	</Typography.Subtitle2>*/}
+			{/*</div>*/}
+			{/*<div>*/}
+			{/*	/!*<Button as={'a'} href={'https://google.com'}>*!/*/}
+			{/*	/!*	Link that looks a button*!/*/}
+			{/*	/!*</Button>*!/*/}
+			{/*	<Button disabled={false} fullWidth={false} icon={<SvgLogout />} variant={'primary'}>*/}
+			{/*		Primary button*/}
+			{/*	</Button>*/}
+			{/*	/!*<Button variant={'secondary'}>Secondary button</Button>*!/*/}
+			{/*</div>*/}
+			{/*<div>*/}
+			{/*	<Input as={'textarea'} label={'Default input'} variant={'default'} />*/}
+			{/*	<Input disabled fullWidth={false} label={'Default input'} variant={'default'} />*/}
+			{/*	<Input disabled={false} fullWidth={false} label={'Default input'} variant={'default'} />*/}
+			{/*	<Input*/}
+			{/*		disabled={false}*/}
+			{/*		errorMessage={'Error'}*/}
+			{/*		fullWidth={false}*/}
+			{/*		label={'Default input'}*/}
+			{/*		variant={'default'}*/}
+			{/*	/>*/}
+			{/*	<span>{'ku'}</span>*/}
+			{/*	<Input*/}
+			{/*		disabled={false}*/}
+			{/*		fullWidth*/}
+			{/*		iconEnd={<SvgEye />}*/}
+			{/*		label={'Password input'}*/}
+			{/*		variant={'password'}*/}
+			{/*	/>*/}
+			{/*	<Input*/}
+			{/*		disabled*/}
+			{/*		fullWidth={false}*/}
+			{/*		iconEnd={<SvgEye />}*/}
+			{/*		label={'Password input'}*/}
+			{/*		variant={'password'}*/}
+			{/*	/>*/}
 
-				<Input
-					disabled={false}
-					fullWidth={false}
-					iconEnd={<SvgClose />}
-					iconStart={<SvgSearch />}
-					label={'Search input'}
-					variant={'search'}
-				/>
+			{/*	<Input*/}
+			{/*		disabled={false}*/}
+			{/*		fullWidth={false}*/}
+			{/*		iconEnd={<SvgClose />}*/}
+			{/*		iconStart={<SvgSearch />}*/}
+			{/*		label={'Search input'}*/}
+			{/*		variant={'search'}*/}
+			{/*	/>*/}
 
-				<Input
-					disabled
-					fullWidth={false}
-					iconEnd={<SvgClose />}
-					iconStart={<SvgSearch />}
-					label={'Search input'}
-					variant={'search'}
-				/>
-				<Input
-					errorMessage={'Error'}
-					fullWidth={false}
-					iconEnd={<SvgClose />}
-					iconStart={<SvgSearch />}
-					label={'Search input'}
-					variant={'search'}
-				/>
-				<Input
-					disabled
-					fullWidth
-					iconEnd={<SvgClose />}
-					iconStart={<SvgSearch />}
-					label={'Search input'}
-					variant={'search'}
-				/>
-			</div>
+			{/*	<Input*/}
+			{/*		disabled*/}
+			{/*		fullWidth={false}*/}
+			{/*		iconEnd={<SvgClose />}*/}
+			{/*		iconStart={<SvgSearch />}*/}
+			{/*		label={'Search input'}*/}
+			{/*		variant={'search'}*/}
+			{/*	/>*/}
+			{/*	<Input*/}
+			{/*		errorMessage={'Error'}*/}
+			{/*		fullWidth={false}*/}
+			{/*		iconEnd={<SvgClose />}*/}
+			{/*		iconStart={<SvgSearch />}*/}
+			{/*		label={'Search input'}*/}
+			{/*		variant={'search'}*/}
+			{/*	/>*/}
+			{/*	<Input*/}
+			{/*		disabled*/}
+			{/*		fullWidth*/}
+			{/*		iconEnd={<SvgClose />}*/}
+			{/*		iconStart={<SvgSearch />}*/}
+			{/*		label={'Search input'}*/}
+			{/*		variant={'search'}*/}
+			{/*	/>*/}
+			{/*</div>*/}
 
-			<div>
-				<CheckboxComponent disabled text={'Checkbox'} />
-			</div>
+			{/*<div>*/}
+			{/*	<CheckboxComponent disabled text={'Checkbox'} />*/}
+			{/*</div>*/}
 
-			{/*<Button as={'div'}>Normal Button</Button>*/}
+			{/*/!*<Button as={'div'}>Normal Button</Button>*!/*/}
 
-			<div>
-				<Card />
-			</div>
+			{/*<div>*/}
+			{/*	<Card />*/}
+			{/*</div>*/}
 		</div>
 	)
 }
